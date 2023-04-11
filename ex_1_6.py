6f7822997252964af72eec279a72433cadb43a50
Roy Cohen <roy.cohen.il@gmail.com>
2023-04-11 10:38:27 +0300

    solved ex_6

--- ex_1_6.py
+++ ex_1_6.py
@@ -5,44 +5,56 @@ from ex_1_3 import break_single_byte_xor_cipher
 import math
 import itertools
 import base64
-
 def main():
     with open ("ex_1_6_input.txt", 'r') as input_file:
         ciphertext = base64.b64decode(input_file.read())
     blocksize = guess_best_blocksize(ciphertext)
     print(f"[+] the guessed blocksize is {blocksize}")
-    transposed_blocks = get_transposed_blocks(ciphertext, blocksize)
-    print(break_single_byte_xor_cipher(transposed_blocks[0]))
+    transposed_cipherblocks = get_transposed_blocks(ciphertext, blocksize)
+    transposed_plainblocks = [break_single_byte_xor_cipher(transposed_cipherblocks[i]) for i in range(len(transposed_cipherblocks))]
+    # print(get_transposed_blocks(b''.join(transposed_plainblocks), 1))
+    print(merge_plaintext(transposed_plainblocks))
+
+
+def merge_plaintext(plaintext_blocks: bytes)-> bytes:
+    plaintext = b""
+    index = 0
+    #combined_size = sum([len(plaintext_blocks[i]) for i in range(len(plaintext_blocks))])
+    #print(combined_size)
+    for i in range(len(plaintext_blocks[0])):
+        for j in range(len(plaintext_blocks)):
+            print(j,i)
+            try:
+                plaintext += chr(plaintext_blocks[j][i]).encode('ascii')
+            except:
+                return plaintext
+
+    return plaintext
 
 def guess_best_blocksize(ciphertext: bytes)-> int:
-    max_blocksize = int(math.floor(len(ciphertext) / 2))
-    lowest_avg = 9999.9
-    lowest_blocksize = 1
+    #max_blocksize = int(math.floor(len(ciphertext) / 2))
+    max_blocksize = 40
+    blocksize_scores = dict()
     for bs in range(3, max_blocksize):
         avg_pair_dist = 0.0
         trimmed_ciphertext = ciphertext[0: len(ciphertext) - len(ciphertext) % bs]
         blocks = split_to_blocks(trimmed_ciphertext, bs)
         block_pairs = list(itertools.combinations(blocks, 2))
-        avg_pair_dist = sum([hamming_distance(pair[0],pair[1]) for pair in block_pairs]) / len(block_pairs)
-        if avg_pair_dist < lowest_avg:
-             lowest_avg = avg_pair_dist
-             lowest_blocksize = bs
-    return lowest_blocksize 
+        avg_pair_dist = (sum([hamming_distance(pair[0],pair[1]) for pair in block_pairs]) / len(block_pairs)) - 3 *bs
+        blocksize_scores[bs] = avg_pair_dist
+
+    return min(blocksize_scores, key=blocksize_scores.get)
 
 def get_transposed_blocks(ciphertext: bytes, blocksize: int) -> list[bytes]:
     blocks = split_to_blocks(ciphertext, blocksize)
     transposed_blocks = [bytearray(b"") for _ in range(blocksize)]
-    # index_looper = itertools.cycle(range(blocksize)) 
     index = 0
     for block in blocks:
         for byte in block:
             transposed_blocks[index % blocksize].append(byte)
             index+=1 
-
     return [bytes(block) for block in transposed_blocks]
 
-    # transposed_rounded_blocks = [bytes(i) for i in zip(*blocks)]
-    # disperse reminding items
     
 
 def split_to_blocks(blob: bytes, blocksize: int) -> Iterable:
@@ -50,11 +62,6 @@ def split_to_blocks(blob: bytes, blocksize: int) -> Iterable:
         yield blob[i: i+blocksize]
 
 def hamming_distance(blob1: bytes, blob2: bytes)-> int:
-    # dist = 0
-    # longer_blob = blob1 if len(blob1) >= len(blob2) else blob2
-    # for i in range(len(longer_blob)):
-    #     dist += abs(hamming_weight_byte(blob1[i]) - hamming_weight_byte(blob2[i]))
-    # return dist 
     return hamming_weight(fixed_xor(blob1, blob2))
 
 def hamming_weight(blob: bytes) -> int:
